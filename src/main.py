f2db40b84c20722f329579c8a603dedf7af50901
billoncapitolhill <contact@thebilloncapitolhill.com>
2024-12-30 15:54:28 -0800

    Update CORS middleware in main.py to allow additional origins for enhanced API accessibility
    This update modifies the CORS middleware configuration in main.py to include new allowed origins, specifically for the production environment. The addition of "https://orca-app-nkgpg.ondigitalocean.app" and "https://www.orca-app-nkgpg.ondigitalocean.app" ensures that the application can be accessed from these domains, improving the flexibility and usability of the API for users. This change supports the ongoing development and deployment of the application.

--- src/main.py
+++ src/main.py
@@ -33,7 +33,11 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://orca-app-nkgpg.ondigitalocean.app",
+        "https://www.orca-app-nkgpg.ondigitalocean.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
